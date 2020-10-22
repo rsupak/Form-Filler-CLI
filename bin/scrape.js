371a60cc41ed757c7e121c5984bb6bd53ffd792f
Richard Supak <richard.supak@rmgnetworks.com>
2020-10-21 20:08:38 -0500

    Automate web scraping for form fields

--- bin/scrape.js
+++ bin/scrape.js
@@ -1,66 +1,49 @@
 var cheerio = require('cheerio');
-var scrape = require('website-scraper');
 var fs = require('fs');
+var SaveToExistingDirectoryPlugin = require('website-scraper-existing-directory');
+var scrape = require('website-scraper');
 
 const AddressBook = 'http://address-book-rsupak.herokuapp.com/entries/new'
 let options = {
-  urls: ['http://address-book-rsupak.herokuapp.com/entries/new'],
-  directory: '../htmls',
+  urls: [ AddressBook ],
+  directory: './html',
+  plugins: [ new SaveToExistingDirectoryPlugin() ],
   recursive: false,
   defaultFilename: "new.html",
   maxDepth: 1
 };
 
-// scrape(options).then((result) => {
-//   console.log("Downloaded");
-//   let htmlFile = fs.readFile('../htmls/new.html', function(err, data) {
-//     return data;
-//   });
-//   console.log(htmlFile)
-// }).catch((err) => {
-//   console.log("Errors occurred ", err);
-// })
+scrape(options).then(result => {
+  let query = cheerio.load(result[0].text);
+  buildConfig(query);
+})
 
-// let htmlFile = fs.readFile('../htmls/new.html', function(err, data) {
-//   return data;
-// });
-
-const htmlFile = fs.readFileSync('../htmls/new.html').toString()
-
-// console.log(htmlFile)
-const $ = cheerio.load(htmlFile);
-// console.log($.html())
-// $('#form_container cur[type=text]').each((index) => {
-//   var cur = $(this);
+const buildConfig = query => {
+  let $ = query;
+  let title = $('head title')[0].children[0].data;
+  let configArray = [
+    'http://address-book-rsupak.herokuapp.com/entries/new',
+    {},
+    {}
+  ]
+  $('input').each(
+    function(index){  
+        var cur = $(this);
+        // console.log('Type: ' + cur.attr('type') + 'Name: ' + cur.attr('name') + 'Value: ' + cur.val());
+        if (cur.attr('type') == 'text') {
+          let accessor = {}
+          accessor["selector"] = "[id=" + cur.attr('id') + "]"
+          accessor["entry"] = '';
+          configArray[1][cur.attr('name')] = accessor
+          // console.log(configObj["0"][1][cur.attr('name')])
+        }
+        if (cur.attr('type') == 'submit') {
+          configArray[2]["submitId"] = ".submit"
+        }
+    }
+  );
   
-//   console.log('Value: ' + cur.val())
-  
-// });
-let title = $("head title")[0].children[0].data
-// console.log(title)
-const configArray = [
-  'http://address-book-rsupak.herokuapp.com/entries/new',
-  {},
-  {}
-]
-
-$('input').each(
-  function(index){  
-      var cur = $(this);
-      // console.log('Type: ' + cur.attr('type') + 'Name: ' + cur.attr('name') + 'Value: ' + cur.val());
-      if (cur.attr('type') == 'text') {
-        let accessor = {}
-        accessor["selector"] = "[id=" + cur.attr('id') + "]"
-        accessor["entry"] = '';
-        configArray[1][cur.attr('name')] = accessor
-        // console.log(configObj["0"][1][cur.attr('name')])
-      }
-      if (cur.attr('type') == 'submit') {
-        configArray[2]["submitId"] = ".submit"
-      }
-  }
-);
-// console.log(configArray)
-const configFile = JSON.parse(fs.readFileSync('../config.json'));
-configFile[title.toLowerCase()] = configArray;
-fs.writeFileSync('../config.json', JSON.stringify(configFile, null, ' '));
+  const configFile = JSON.parse(fs.readFileSync('../config.json'));
+  configFile[title.toLowerCase()] = configArray;
+  fs.writeFileSync('../config.json', JSON.stringify(configFile, null, ' '));
+}
